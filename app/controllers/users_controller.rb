35f8ad59f225a3d0682696e31aa0f03fac0a61ec
mack02 <makuuchi23@gmail.com>
2022-02-21 07:19:07 +0000

    kadai teisyutugo1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
     @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to tasks_path
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
