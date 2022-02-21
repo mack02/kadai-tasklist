586b3809c968e4c5457fef29832d07b5a46ae294
mack02 <makuuchi23@gmail.com>
2022-02-21 03:09:09 +0000

    kadai kari2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = current_user.tasks
+    @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
   end
 
   def show
